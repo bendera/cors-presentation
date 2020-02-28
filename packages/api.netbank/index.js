cfa123fe95d3d51b71450db95757901c66d48c12
bendera <szoftverhiba@gmail.com>
2020-02-28 02:12:05 +0100

    session handling

--- packages/api.netbank/index.js
+++ packages/api.netbank/index.js
@@ -1,20 +1,61 @@
 const express = require('express');
 const cors = require('cors');
+const cookieSession = require('cookie-session');
+const bodyParser = require('body-parser');
+
+const YEAR = 24 * 60 * 60 * 1000 * 365;
+
 const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(express.static('public'));
-app.use(cors());
+app.use(cors({
+  origin: true,
+  credentials: true,
+}));
+app.use(cookieSession({
+  name: 'session',
+  keys: ['foo'],
+  maxAge: YEAR,
+}));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
 
 app.get('/', (req, res) => res.send('Hello World!'));
 
-app.get('/userinfo', (req, res) => {
+app.post('/user', (req, res) => {
+  if (req.body.username === 'admin' && req.body.password === 'admin') {
+    req.session.loggedIn = true;
+  }
+
+  const loggedIn = req.session.loggedIn || false;
+
   res.json({
-    username: 'johndoe',
-    name: 'john Doe',
-    email: 'johndoe@gmailcom',
-    accountBalance: '999'
+    loggedIn: loggedIn,
   });
+});
+
+app.get('/user', (req, res) => {
+  const loggedIn = req.session.loggedIn || false;
+
+  req.session.views = (req.session.views || 0) + 1;
+
+  if (loggedIn) {
+    res.json({
+      loggedIn,
+      username: 'johndoe',
+      name: 'john Doe',
+      email: 'johndoe@gmailcom',
+      accountBalance: '999',
+      views: req.session.views,
+    });
+  } else {
+    res.json({
+      loggedIn
+    });
+  }
 });
 
 app.options('*', cors());
