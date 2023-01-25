d1582c03321d3523fb7a76c1d0c21793aa037d11
bendera <szoftverhiba@gmail.com>
2023-01-25 23:50:54 +0100

    change urls

--- packages/api.netbank/index.js
+++ packages/api.netbank/index.js
@@ -10,7 +10,7 @@ const port = process.env.PORT || 3000;
 
 app.use(express.static('public'));
 app.use(cors({
-  origin: 'http://netbank.localhost',
+  origin: 'https://netbank.test',
 }));
 app.use(cookieSession({
   name: 'session',
